9062aa0967f61e9b486f3627a3a70f1e68d77857
ShaymaREBHI <shayma.rebhi@esprit.tn>
2022-03-01 00:19:03 +0100

    CRUD

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(config.mongo.uri,
 
 var indexRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
